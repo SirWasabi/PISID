a9e264de15526521ba21ef8da04c37fd00f40854
SirWasabi <vpereira95@gmail.com>
2024-04-20 12:17:12 +0100

    Mongo - Java

---
+++ 3 - JSON para MYSQL/Mains/MongoTempsToJava/src/MongoTempsToJava.java
@@ -0,0 +1,66 @@
+import java.util.Properties;
+import java.io.*;
+
+import org.bson.Document;
+
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoTempsToJava {
+    
+    static MongoClient mongoClient;
+    static MongoDatabase db;
+    static MongoCollection<Document> mongocol;
+	static String mongo_user = new String();
+	static String mongo_password = new String();
+	static String mongo_address = new String();
+    static String mongo_host = new String();
+    static String mongo_replica = new String();
+	static String mongo_database = new String();
+    static String mongo_collection = new String();
+	static String mongo_authentication = new String(); 
+	
+    public static void main(String[] args) {
+        try {
+            Properties p = new Properties();
+            p.load(new FileInputStream("src/MongoTempsToJava.ini"));
+			mongo_address = p.getProperty("mongo_address");
+            mongo_user = p.getProperty("mongo_user");
+            mongo_password = p.getProperty("mongo_password");						
+            mongo_replica = p.getProperty("mongo_replica");
+            mongo_host = p.getProperty("mongo_host");
+            mongo_database = p.getProperty("mongo_database");
+            mongo_authentication = p.getProperty("mongo_authentication");			
+            mongo_collection = p.getProperty("mongo_collection");
+        } catch (Exception e) {
+            System.out.println("Error reading MongoTempsToJava.ini file " + e);
+        }
+        
+        new MongoTempsToJava().connectMongo();
+    }
+
+    public void connectMongo() {
+		String mongoURI = new String();
+		mongoURI = "mongodb://";		
+		if (mongo_authentication.equals("true")) mongoURI = mongoURI + mongo_user + ":" + mongo_password + "@";		
+		mongoURI = mongoURI + mongo_address;		
+		if (!mongo_replica.equals("false")) 
+			if (mongo_authentication.equals("true")) mongoURI = mongoURI + "/?replicaSet=" + mongo_replica+"&authSource=admin";
+			else mongoURI = mongoURI + "/?replicaSet=" + mongo_replica;		
+		else
+			if (mongo_authentication.equals("true")) mongoURI = mongoURI  + "/?authSource=admin";			
+		try (MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoURI))) {
+            db = mongoClient.getDatabase(mongo_database);
+            mongocol = db.getCollection(mongo_collection);
+
+            System.out.println(mongocol.count());
+            FindIterable<Document> doc = mongocol.find();
+            for(Document row : doc) {
+                System.out.println(row.toJson());
+            }
+        }
+    }
+
+}
